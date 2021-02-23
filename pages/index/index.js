6d7f09eb766e8e2b989fae54b6b10acfb1df3722
huangsl <1197784768@qq.com>
2021-02-23 16:50:10 +0800

    选择图片

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,36 +9,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list: []
+    list: [],
+    imageUrl: ''
   },
 
-  bindGetUserInfo(e){
-    console.log(e.detail.userInfo);
+  handleImageClick(){
+    wx.chooseImage({
+      success: (res) => {
+        const path = res.tempFilePaths[0];
+        this.setData({
+          imageUrl: path
+        })
+      }
+    })
   },
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.getSetting({
-      success (res){
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: function(res) {
-              console.log(res.userInfo)
-            }
-          })
-        }
-      }
-    })
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
